775b58c5407824193295ef98d1370a381b729923
Marco Cardinali <marco.cardinali94@yahoo.it>
2020-05-29 13:20:50 +0200

    Addedd the jacknfie analysis.

--- Pylib/metro_gauss_test.py
+++ Pylib/metro_gauss_test.py
@@ -1,13 +1,13 @@
 import numpy as np
 import statol as stat
-from random import * 
+from random import random 
 
 
 therm = 10000
 delta = 0.1
 
 #### Gaussian calculator ####
-mu = 5
+mu = 0
 sigma = 1
 
 def gauss(x):
@@ -17,27 +17,37 @@ def gauss(x):
 sample=[]
 sample.append(0)
 for i in range(10000000):
-	if sample[i]-delta >0:
-		candidate = sample[i] - delta + random()*2*delta
-	else:
-		candidate = sample[i] + random()*delta
-	P_old = gauss(sample[i])	
-	P_new = gauss(candidate)
+    candidate = sample[i] - delta + random()*2*delta
+    
+    P_old = gauss(sample[i])	
+    P_new = gauss(candidate)
 
-	coin = random()
-	if P_new > P_old:
-		sample.append(candidate)
-	else:
-		if  coin < P_new/P_old:
-			sample.append(candidate)
-		else:
-			sample.append(sample[i])
-#	if i > therm:
-#		print(sample[i+1])  
+    coin = random()
+    if P_new > P_old:
+        sample.append(candidate)
+    else:
+       if coin < P_new/P_old:
+           sample.append(candidate)
+       else:
+           sample.append(sample[i])
+	#f i > therm:
+	#	print(sample[i+1], file=open('MC_story.dat', "a"))  
 
 
 sample = np.array(sample)
+sample = sample[10*therm:]
 
-for i in range(15):
-	aux=stat.block_data(sample, 2**(i))
-	print(np.mean(aux), np.std(aux, ddof=1)/np.sqrt(len(aux)), len(aux))
+def x4(data):
+    return data*data*data*data
+
+def x2(data):
+    return data*data
+
+def binder(args):
+    return args[0]/(3*(args[1]**2))
+
+for i in range(20):
+    mean, err = stat.jack_for_composite(binder, 2**i, [x4, sample], [x2, sample])
+    print(mean, err)
+    #aux=stat.block_data(sample, 2**(i))
+	#print(np.mean(aux), np.std(aux, ddof=1)/np.sqrt(len(aux)), len(aux))
--- Pylib/statol.py
+++ Pylib/statol.py
@@ -1,13 +1,13 @@
-###############################
-#							  #
-#  Library for Data Analysis  #
-#							  #
-###############################
+################################
+#			                   #
+#  Library for Data Analysis   #
+#			                   #
+################################
 import numpy as np
 
 
 
-# takes in input np.array(data) e returns the blocked array with block length N
+# takes in input np.array(data) annd a size N. It returns the blocked array with block length N
 def block_data(data, N):
 	if N <= 0:
 		print("ERROR: the size of the block must be > 0!")
@@ -24,3 +24,104 @@ def block_data(data, N):
 	shaped_data = cutted_data.reshape(blocked_lenght, N)
 	
 	return np.mean(shaped_data, axis = 1)
+
+
+def block_sum(data, N):
+    if N <= 0:
+        print("ERROR: the size of the block must be > 0!")
+        exit(1)
+    
+    blocked_lenght = int(len(data)/N)
+    
+    if blocked_lenght <= 0:
+        print("ERROR: the  blocksize is too big!")
+        exit(1)
+
+    to_be_cut = len(data)%N
+    cutted_data = data[to_be_cut:]
+    shaped_data = cutted_data.reshape(blocked_lenght, N)
+	
+    return np.sum(shaped_data, axis = 1)
+
+
+def jack_for_single(func, data, N):
+    if N <= 0:
+        print("ERROR: the size of the block must be >0!")
+        exit(1)
+        
+    num_blocks = int(len(data)/N)
+
+    if num_blocks <=0:
+        print("ERROR: the blocksize is too big!")
+        exit(1)
+        
+    to_be_cut = len(data)%N
+    cutted_data = data[to_be_cut:]
+    
+    eval_data = func(cutted_data)
+ 
+    mean_value = np.mean(eval_data)
+    
+    # jacknife error procedure
+    sums = block_sum(eval_data, N)
+    total_sum = np.sum(sums)
+    repeted_total_sum = np.tile(total_sum, num_blocks)
+    jack_samples = (repeted_total_sum - sums)/((num_blocks - 1)*N)
+    
+    return mean_value, np.std(jack_samples)*np.sqrt(num_blocks - 1)
+    
+
+def jack_for_composite(comp_func, N, *args):
+    """ Compute the jacknife analysis on comp_func(<func(data)>) 
+        args is a list composed by list of the form [func, data].
+        In the analysis N is used as the block size"""
+    
+    if N <= 0:
+        print("ERROR: the size of the block must be >0!")
+        exit(1)
+        
+    # jakcnife error list
+    jack_list = []
+    
+    # mean values list
+    mean_values_list = []
+    
+    for el in args:
+        func, data = el
+        
+        num_blocks = int(len(data)/N)
+
+        if num_blocks <=0:
+            print("ERROR: the blocksize is too big!")
+            exit(1)
+        
+        to_be_cut = len(data)%N
+        cutted_data = data[to_be_cut:]
+    
+        eval_data = func(cutted_data)
+ 
+        mean = np.mean(eval_data)
+    
+        # jacknife error procedure
+        sums = block_sum(eval_data, N)
+        total_sum = np.sum(sums)
+        repeted_total_sum = np.tile(total_sum, num_blocks)
+        jack_samples = (repeted_total_sum - sums)/((num_blocks - 1)*N)
+        
+        jack_list.append(jack_samples)
+        mean_values_list.append(mean)        
+
+    composite_jack = comp_func(jack_list)
+    
+    mean_value = num_blocks*comp_func(mean_values_list) - (num_blocks - 1)*np.mean(composite_jack)   
+    err = np.std(composite_jack)*np.sqrt(num_blocks - 1)
+    
+    return mean_value, err
+    
+
+
+    
+    
+    
+        
+    
